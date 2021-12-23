d634301003ac44857aa5e16125d8b0a0bb70f83d
hzhzhai <hzhzhai@163.com>
2021-12-23 22:57:09 +0800

    create module

---
+++ blogs/src/test/java/my/hzhzhai/blogs/BlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.hzhzhai.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
